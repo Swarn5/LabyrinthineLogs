1240e766cb13aed20e902070ca75a54560b30d00
Swarn Shekhar <swarnshekhar5@gmail.com>
2023-07-26 13:55:24 +0530

    initial index page created

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './Header';
+import Layout from './Layout';
+import Post from './Post';
+import { Route, Routes } from "react-router-dom";
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element = {<Layout/>}>
+      <Route index element={<IndexPage/>} />
+      <Route path={'/login'} element={<LoginPage/>} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2023/07/x-twitter-1.jpg?w=850&h=492&crop=1" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Twitter handle says no one reached out ahead of Twitter's rebranding to 'X'</h2>
+          <p className="info">
+            <a href="" className="author">Swarn Shekhar</a>
+            <time>2023-07-26 01:02</time>
+          </p>
+          <p className="summary">Twitter's rebranding to X has not been faring well. In addition to the haphazard rollout, which saw parts of the site referencing “X” while other parts still implored you to “search Twitter” or push a blue button to “Tweet,” the company also didn't secure the intellectual property rights to the X brand, Reuters reported.</p>
+        </div>
+      </div>
+    );
+}
