2a40168f8256e3120b39afba64329e60844f83a0
Swarn Shekhar <swarnshekhar5@gmail.com>
2023-08-10 15:09:06 +0530

    index and login page fixed

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const secret = 'dfmvndrktn4l6n4mbdf5jktbkvbe56bvk';
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://swarnshekhar5:YR1PsQ46i8OZLUla@cluster0.pyvua1b.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb://Swarnshekhar:q3NpNBHjr6vIBRvZ@ac-siwdeel-shard-00-00.pyvua1b.mongodb.net:27017,ac-siwdeel-shard-00-01.pyvua1b.mongodb.net:27017,ac-siwdeel-shard-00-02.pyvua1b.mongodb.net:27017/?ssl=true&replicaSet=atlas-wg4rgb-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
@@ -46,3 +46,4 @@ app.post('/login', async (req,res) => {
 app.listen(4000);
 //mongodb+srv://swarnshekhar5:YR1PsQ46i8OZLUla@cluster0.pyvua1b.mongodb.net/?retryWrites=true&w=majority
 //YR1PsQ46i8OZLUla
+//q3NpNBHjr6vIBRvZ
--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,22 @@ import './App.css';
 import Header from './Header';
 import Layout from './Layout';
 import Post from './Post';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,RouterProvider } from "react-router-dom";
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <Routes>
+     <Routes>
+    
       <Route path='/' element = {<Layout/>}>
-      <Route index element={<IndexPage/>} />
+      { <Route index element={<IndexPage/>} /> }
       <Route path='/login' element={<LoginPage/>} />
-      <Route path='/register' element={<RegisterPage/>} />
+      {<Route path='/register' element={<RegisterPage/>} />}
       </Route>
-    </Routes>
+      
+     </Routes>
   );
 }
 
