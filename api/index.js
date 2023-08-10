6de1c4e2c18e05c3f9fd626b1d0d036c739ea61e
Swarn Shekhar <swarnshekhar5@gmail.com>
2023-08-10 18:42:39 +0530

    connection bug fixes

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const secret = 'dfmvndrktn4l6n4mbdf5jktbkvbe56bvk';
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
-mongoose.connect('mongodb://Swarnshekhar:q3NpNBHjr6vIBRvZ@ac-siwdeel-shard-00-00.pyvua1b.mongodb.net:27017,ac-siwdeel-shard-00-01.pyvua1b.mongodb.net:27017,ac-siwdeel-shard-00-02.pyvua1b.mongodb.net:27017/?ssl=true&replicaSet=atlas-wg4rgb-shard-0&authSource=admin&retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://Swarnshekhar:UZyfHLVq38OOZtiJ@cluster0.pyvua1b.mongodb.net/');
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
@@ -44,6 +44,6 @@ app.post('/login', async (req,res) => {
 });
 
 app.listen(4000);
+console.log("connected succesfully")
 //mongodb+srv://swarnshekhar5:YR1PsQ46i8OZLUla@cluster0.pyvua1b.mongodb.net/?retryWrites=true&w=majority
-//YR1PsQ46i8OZLUla
-//q3NpNBHjr6vIBRvZ
+// UZyfHLVq38OOZtiJ
